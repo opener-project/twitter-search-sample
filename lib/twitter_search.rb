08794b6baea65055f0b47ae62755d92755fdb943
Giannis Melidis <giannismelidis@olery.com>
2013-07-02 13:57:52 +0200

    updated so that it stores any valid result

--- lib/twitter_search.rb
+++ lib/twitter_search.rb
@@ -22,7 +22,7 @@ class TwitterSearch
   end
 
   def fetch
-    Twitter.search("#{hash_tags}", count: 1500, result_type: "recent").results.each do |status|
+    Twitter.search("#{hash_tags}", count: 500, result_type: "recent").results.each do |status|
       tweet = clean_tweet(status.text)
       output = get_kaf(tweet)
       puts tweet
@@ -45,20 +45,48 @@ class TwitterSearch
     client = HTTPClient.new
     
     language_identifier = "http://opener.olery.com/language-identifier"
-    res = client.post(language_identifier, :input => tweet, :kaf => true )
-    tokenizer = "http://opener.olery.com/tokenizer"
-    res = client.post(tokenizer, :input => res.body, :kaf => true)
-    pos_tagger = "http://opener.olery.com/POS-tagger"
-    res = client.post(pos_tagger, :input => res.body)
-    polarity_tagger = "http://opener.olery.com/polarity-tagger"
-    res = client.post(polarity_tagger, :input => res.body) 
-    ner = "http://opener.olery.com/ner"
-    res = client.post(ner, :input => res.body)
-    opinion_detector = "http://opener.olery.com/opinion-detector"
-    res = client.post(opinion_detector, :input => res.body)
-  
-    File.open(["tmp/", create_uuid, ".txt"].join, 'w') { |file| file.write(res.body) }
+    output = client.post(language_identifier, :input => tweet, :kaf => true )
+    if no_error?(output)
+      res = output
+      tokenizer = "http://opener.olery.com/tokenizer"
+      output = client.post(tokenizer, :input => res.body, :kaf => true)
+    end
+    if no_error?(output)
+      res = output
+      pos_tagger = "http://opener.olery.com/POS-tagger"
+      output = client.post(pos_tagger, :input => res.body)
+    end
+    if no_error?(output)
+      res = output
+      polarity_tagger = "http://opener.olery.com/polarity-tagger"
+      output = client.post(polarity_tagger, :input => res.body) 
+    end
+    if no_error?(output)
+      res = output
+      ner = "http://opener.olery.com/ner"
+      output = client.post(ner, :input => res.body)
+    end
+    if no_error?(output)
+      res = output
+      opinion_detector = "http://opener.olery.com/opinion-detector"
+      output = client.post(opinion_detector, :input => res.body)
+    end
     
+    if no_error?(output)
+      res = output
+    end
+    unless res.nil?
+      File.open(["tmp/", create_uuid, ".txt"].join, 'w') { |file| file.write(res.body) }
+    end
+    
+  end
+  
+  def no_error?(output)
+    if output.status == 200 && output.body.index('Internal Server Error').nil? && !output.nil?
+      true
+    else
+      false
+    end
   end
   
   def create_uuid
